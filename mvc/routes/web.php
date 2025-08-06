ee9924bbc1777e4aee669f9f61cf95a91bd13fa1
= <chanmichou37@gmail.com>
2025-08-06 18:53:37 -0400

    Mise à jour des routes

--- mvc/routes/web.php
+++ mvc/routes/web.php
@@ -1,7 +1,27 @@
 <?php
 use App\Routes\Route;
+use App\Controllers\HomeController;
+use App\Controllers\MemberController;
+use App\Controllers\UserController;
+use App\Controllers\UserIndexController;  
+use App\Controllers\AuthController; 
+
 Route::get('/', 'HomeController@index');
-Route::get('/home', 'HomeController@index');
-Route::get('/home/index', 'HomeController@index');
+//Route::get('/home', 'HomeController@index');
+//Route::get('/home/index', 'HomeController@index');
+
+Route::get('/user/create', 'UserController@create');
+Route::post('/user/store', 'UserController@store');
+Route::get('/users', 'UserIndexController@index');
+
+Route::get('/members', 'MemberController@index');
+Route::get('/member/create', 'MemberController@create');
+Route::post('/member/create', 'MemberController@store');
+
+// Route pour l'authentification
+
+Route::get('/login', 'AuthController@index');
+Route::post('/login', 'AuthController@store');
+
 
 Route::dispatch();
