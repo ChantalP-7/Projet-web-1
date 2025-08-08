79c299692d3698505a2f8e92680b02e62d8e831e
= <chanmichou37@gmail.com>
2025-08-07 20:50:49 -0400

    Corrections diverses, révision de la base de données, etc.

--- mvc/stampee.sql
+++ mvc/stampee.sql
@@ -1,37 +1,15 @@
 create database stampee;
 
-Create table utilisateur (
+Create table membre (
     id int not null AUTO_INCREMENT,
-    prenom VARCHAR(45),
-    nom VARCHAR(45),
-    nomUtilisateur VARCHAR(45),
-    courriel VARCHAR(45),
-    password VARCHAR(45),
-    idRole int not null,
+    prenom VARCHAR(45) not null,
+    nom VARCHAR(45) not null,    
+    courriel VARCHAR(45) not null,
+    telephone VARCHAR(45),
+    username VARCHAR(45) not null UNIQUE,
+    password VARCHAR(255) not null,
     PRIMARY KEY (id),
-    UNIQUE KEY courriel_unique (courriel),
-    KEY `idRole` (`idRole`),
-CONSTRAINT `utilisateur_ibfk_1` FOREIGN KEY (`idRole`) REFERENCES `role` (`id`)
-)
-
-CREATE TABLE IF NOT EXISTS `membre` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `pseudonyme` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-    `telephone` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-    `idUtilisateur` int NOT NULL,
-    PRIMARY KEY (`id`),
-    KEY `idUtilisateur` (`idUtilisateur`),
-    CONSTRAINT `membre_ibfk_1` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateur` (`id`)
-)
-
-CREATE TABLE IF NOT EXISTS `admin` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `nom` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-    `telephone` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-    `idUtilisateur` int NOT NULL,
-    PRIMARY KEY (`id`),
-    KEY `idUtilisateur` (`idUtilisateur`),
-    CONSTRAINT `admin_ibfk_2` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateur` (`id`)
+    UNIQUE KEY username_unique (username)
 )
 
 CREATE TABLE IF NOT EXISTS `timbre` (
@@ -39,16 +17,25 @@ CREATE TABLE IF NOT EXISTS `timbre` (
     `lot` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
     `nom` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
     `datePublication` date NOT NULL,
-    `PaysOrigine` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `couleur` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `image` BLOB DEFAULT NULL,
-    `condition` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `dimension` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-    `certifie` TINYINT DEFAULT NULL,
+    `idImage` int not NULL,
+    `idPaysOrigine` int not NULL,
+    `idCondition` int not NULL,
+    `idCouleur` int not NULL,
+    `idDimension` int not NULL,
     `idMembre` int NOT NULL
     PRIMARY KEY (`id`),
+    KEY `idImage` (`idImage`),  
+    KEY `idPaysOrigine` (`idPaysOrigine`),  
+    KEY `idCouleur` (`idCouleur`),  
+    KEY `idCondition` (`idCondition`),  
+    KEY `idDimension` (`idDimension`),  
     KEY `idMembre` (`idMembre`),  
-    CONSTRAINT `timbre_ibfk_1` FOREIGN KEY (`idMembre`) REFERENCES `membre` (`id`)
+    CONSTRAINT `timbre_ibfk_1` FOREIGN KEY (`idImage`) REFERENCES `image` (`id`)
+    CONSTRAINT `timbre_ibfk_2` FOREIGN KEY (`idPaysOrigine`) REFERENCES `paysOrigine` (`id`)
+    CONSTRAINT `timbre_ibfk_3` FOREIGN KEY (`idCouleur`) REFERENCES `couleur` (`id`)
+    CONSTRAINT `timbre_ibfk_4` FOREIGN KEY (`idCondition`) REFERENCES `condition` (`id`)
+    CONSTRAINT `timbre_ibfk_5` FOREIGN KEY (`idDimensioin`) REFERENCES `dimension` (`id`)
+    CONSTRAINT `timbre_ibfk_6` FOREIGN KEY (`idMembre`) REFERENCES `membre` (`id`)
 )
 
 CREATE TABLE IF NOT EXISTS `enchere` (
@@ -61,7 +48,7 @@ CREATE TABLE IF NOT EXISTS `enchere` (
     `idTimbre` int NOT NULL
     PRIMARY KEY (`id`),
     KEY `idTimbre` (`idTimbre`),  
-    CONSTRAINT `enchere_ibfk_1` FOREIGN KEY (`idTimbre`) REFERENCES `timbre` (`id`)
+    CONSTRAINT `enchere_ibfk_7` FOREIGN KEY (`idTimbre`) REFERENCES `timbre` (`id`)
 )
 
 CREATE TABLE IF NOT EXISTS `miseEnchere` (
@@ -73,8 +60,47 @@ CREATE TABLE IF NOT EXISTS `miseEnchere` (
     PRIMARY KEY (`id`),
     KEY `idMembre` (`idMembre`),
     KEY `idEnchere` (`idEnchere`),
-    CONSTRAINT `enchereMembre_ibfk_1` FOREIGN KEY (`idMembre`) REFERENCES `membre` (`id`),
-    CONSTRAINT `enchereMembre_ibfk_2` FOREIGN KEY (`idEnchere`) REFERENCES `enchere` (`id`)
+    PRIMARY KEY (`idMembre`),
+    PRIMARY KEY (`idEnchere`),
+    CONSTRAINT `miseEnchere_ibfk_8` FOREIGN KEY (`idMembre`) REFERENCES `membre` (`id`),
+    CONSTRAINT `miseEnchere_ibfk_9` FOREIGN KEY (`idEnchere`) REFERENCES `enchere` (`id`)
+)
+
+CREATE TABLE favori (
+    KEY `idMembre` (`idMembre`),
+    KEY `idEnchere` (`idEnchere`),
+    CONSTRAINT `favori_ibfk_12` FOREIGN KEY (`idMembre`) REFERENCES `membre` (`id`),
+    CONSTRAINT `favori_ibfk_13` FOREIGN KEY (`idEnchere`) REFERENCES `enchere` (`id`)
+)
+
+CREATE TABLE paysOrigine (
+    id int not null AUTO_INCREMENT,
+    pays VARCHAR(45) not null,
+    PRIMARY KEY (`id`),
+)
+
+CREATE TABLE couleur (
+    id int not null AUTO_INCREMENT,
+    couleur VARCHAR(45) not null,
+    PRIMARY KEY (`id`),
+)
+
+CREATE TABLE dimension (
+    id int not null AUTO_INCREMENT,
+    dimension VARCHAR(45) not null,
+    PRIMARY KEY (`id`),
+)
+
+CREATE TABLE laCondition (
+    id int not null AUTO_INCREMENT,
+    laCondition VARCHAR(45) not null,
+    PRIMARY KEY (`id`),
+)
+
+CREATE TABLE imageTimbre (
+    id int not null AUTO_INCREMENT,
+    image VARCHAR(255) not null,
+    PRIMARY KEY (`id`),
 )
 
 CREATE TABLE IF NOT EXISTS `commentaire` (
@@ -87,6 +113,6 @@ CREATE TABLE IF NOT EXISTS `commentaire` (
   PRIMARY KEY (`id`),
   KEY `idMembre` (`idMembre`),
   KEY `idEnchere` (`idEnchere`),
-  CONSTRAINT `enchereMembre_ibfk_1` FOREIGN KEY (`idMembre`) REFERENCES `membre` (`id`),
-CONSTRAINT `enchereMembre_ibfk_2` FOREIGN KEY (`idEnchre`) REFERENCES `enchere` (`id`)
+  CONSTRAINT `enchereMembre_ibfk_10` FOREIGN KEY (`idMembre`) REFERENCES `membre` (`id`),
+CONSTRAINT `enchereMembre_ibfk_11` FOREIGN KEY (`idEnchre`) REFERENCES `enchere` (`id`)
 )
